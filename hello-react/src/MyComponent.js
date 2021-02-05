93176641d3892a2afb43fcd14a0ff19da3a69b8a
dgkim2 <dgkim@bsgglobal.com>
2021-02-05 16:19:02 +0900

    버전업

---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+class MyComponent extends Component {
+  static defaultProps = {
+    name: "Default Name",
+    children: <b>default Children</b>,
+  };
+  static propTypes = {
+    name: propTypes.string,
+    favoriteNumber: propTypes.number.isRequired,
+  };
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        <div>props : {name}</div>
+        <div>Children : {children}</div>
+        <br />
+        좋아하는숫자 : {favoriteNumber}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
