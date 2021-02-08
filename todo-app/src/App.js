52badafd4722bfedf04740b4d38e8ddc75bd4afe
dgkim2 <dgkim@bsgglobal.com>
2021-02-08 14:16:21 +0900

    react-virtualized 추가

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,36 +3,51 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoList from "./components/TodoList";
 
-const App = () => {
-    const [todos, setTodos] = useState([
-        { id: 1, text: "리액트 기초 알아보기", checked: true },
-        { id: 2, text: "컴포넌트 스타일링하기", checked: true },
-        { id: 3, text: "일정관리 앱 만들어보기", checked: false },
-    ]);
-    const nextId = useRef(4); //ref 를 사용하여 변수 담기
+function createBulkTodos() {
+    const array = [];
+    for (let i = 1; i <= 2500; i++) {
+        array.push({
+            id: i,
+            text: `할 일 ${i}`,
+            checked: false,
+        });
+    }
+    return array;
+}
 
-    const onInsert = useCallback(
-        (text) => {
-            const todo = {
-                id: nextId.current,
-                text,
-                checked: false,
-            };
-            setTodos(todos.concat(todo));
-            nextId.current += 1; //nextId 1씩 더하기
-        },
-        [todos]
-    );
+const App = () => {
+    const [todos, setTodos] = useState(createBulkTodos);
+    const nextId = useRef(2501); //ref 를 사용하여 변수 담기
+
+    const onInsert = useCallback((text) => {
+        const todo = {
+            id: nextId.current,
+            text,
+            checked: false,
+        };
+        setTodos((todos) => todos.concat(todo));
+        nextId.current += 1; //nextId 1씩 더하기
+    }, []);
     const onRemove = useCallback(
         (id) => {
             setTodos(todos.filter((todo) => todo.id !== id));
         },
         [todos]
     );
+    const onToggle = useCallback(
+        (id) => {
+            setTodos(
+                todos.map((todo) =>
+                    todo.id === id ? { ...todo, checked: !todo.checked } : todo
+                )
+            );
+        },
+        [todos]
+    );
     return (
         <TodoTemplate>
             <TodoInsert onInsert={onInsert} />
-            <TodoList todos={todos} onRemove={onRemove} />
+            <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
         </TodoTemplate>
     );
 };
