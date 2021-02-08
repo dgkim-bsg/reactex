bf34e4d1e3069e61af058e8ad61209709f490834
dgkim2 <dgkim@bsgglobal.com>
2021-02-08 14:35:11 +0900

    소스 추가

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -7,11 +7,14 @@ import {
 import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
     const { id, text, checked } = todo;
     return (
-        <div className="TodoListItem">
-            <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)} >
+        <div className="TodoListItem-virtualized" style={style}>
+            <div
+                className={cn("checkbox", { checked })}
+                onClick={() => onToggle(id)}
+            >
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 <div className="text">{text}</div>
             </div>
@@ -22,4 +25,7 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
     );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+    TodoListItem,
+    (prevProps, nextProps) => prevProps.todo === nextProps.todo
+);
