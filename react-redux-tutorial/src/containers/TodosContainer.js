66c15e06ddcbea61441ce92f62adf534ff6a647d
dgkim2 <dgkim@bsgglobal.com>
2021-02-09 17:59:45 +0900

    소스 추가

--- react-redux-tutorial/src/containers/TodosContainer.js
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { connect } from "react-redux";
 import { changeInput, insert, toggle, remove } from "../modules/todos";
 import Todos from "../components/Todos";
