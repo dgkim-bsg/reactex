aefd78c68aa8170e7015c7a310943ba8dbea749c
dgkim <dgkim>
2021-02-13 15:35:47 +0900

    소스 정리

--- black-dashboard/src/index.js
+++ black-dashboard/src/index.js
@@ -19,28 +19,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import AdminLayout from "layouts/Admin/Admin.js";
-import RTLLayout from "layouts/RTL/RTL.js";
+import AdminLayout from "./layouts/Admin/Admin.js";
 
-import "assets/scss/black-dashboard-react.scss";
-import "assets/demo/demo.css";
-import "assets/css/nucleo-icons.css";
+import "./assets/scss/black-dashboard-react.scss";
+import "./assets/demo/demo.css";
+import "./assets/css/nucleo-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";
 
 ReactDOM.render(
-  <ThemeContextWrapper>
-    <BackgroundColorWrapper>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
-          <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
-          <Redirect from="/" to="/admin/dashboard" />
-        </Switch>
-      </BrowserRouter>
-    </BackgroundColorWrapper>
-  </ThemeContextWrapper>,
-  document.getElementById("root")
+    <ThemeContextWrapper>
+        <BackgroundColorWrapper>
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path="/admin"
+                        render={(props) => <AdminLayout {...props} />}
+                    />
+                    <Redirect from="/" to="/admin/dashboard" />
+                </Switch>
+            </BrowserRouter>
+        </BackgroundColorWrapper>
+    </ThemeContextWrapper>,
+    document.getElementById("root")
 );
