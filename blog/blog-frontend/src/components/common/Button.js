24fc925d4443fc2c59ccefa515c2097710c84f23
dgkim <dgkim>
2021-02-14 00:59:33 +0900

    소스 추가

--- blog/blog-frontend/src/components/common/Button.js
+++ blog/blog-frontend/src/components/common/Button.js
@@ -3,7 +3,7 @@ import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 import palette from "../../lib/styles/palette";
 
-const StyledButton = styled.button`
+const buttonStyle = css`
     border: none;
     border-radius: 4px;
     font-size: 1rem;
@@ -17,8 +17,34 @@ const StyledButton = styled.button`
     &:hover {
         background: ${palette.gray[6]};
     }
+
+    ${(props) =>
+        props.fullWidth &&
+        css`
+            padding-top: 0.75rem;
+            padding-bottom: 0.75rem;
+            width: 100%;
+            font-size: 1.125rem;
+        `}
+
+    ${(props) =>
+        props.cyan &&
+        css`
+            background: ${palette.cyan[5]};
+            &:hover {
+                background: ${palette.cyan[4]};
+            }
+        `}
 `;
 
-const Button = (props) => <StyledButton {...props} />;
+const StyledButton = styled.button`
+    ${buttonStyle}
+`;
+
+const StyledLink = styled(Link)`
+    ${buttonStyle}
+`;
+
+const Button = (props) => <StyledButton {...props} />
 
 export default Button;
