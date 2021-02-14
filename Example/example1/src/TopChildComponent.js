242c9c70d1cc835be2775a6a8f7fc6547b733134
dgkim <dgkim>
2021-02-15 02:22:15 +0900

    소스 추가

--- Example/example1/src/TopChildComponent.js
+++ Example/example1/src/TopChildComponent.js
@@ -1,16 +1,13 @@
 import React, {useState} from 'react';
 const TopChildComponent = (props) => {
     const [count, setCount] = useState(0);
-    function countChange(e) {
-        setCount(e.target.value);
-    }
     const onClickAction = () => {
         props.onClickAction(count);
     };
     return (
         <div>
             <p>TopChildComponent</p>
-            <input type="text" value={count} onChange={countChange}/>
+            <input type="text" value={count} onChange={(e) => {setCount(e.target.value)}} />
             <button onClick={onClickAction}>더하기</button>
         </div>
     );
