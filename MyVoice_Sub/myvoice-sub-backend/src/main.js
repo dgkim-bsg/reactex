ba1ed2a80a36baa4c4bacda17cc60207fa9aeddb
dgkim <dgkim>
2021-02-17 00:43:35 +0900

    추가 소스

--- MyVoice_Sub/myvoice-sub-backend/src/main.js
+++ MyVoice_Sub/myvoice-sub-backend/src/main.js
@@ -3,7 +3,7 @@ import Koa from "koa";
 import Router from "koa-router";
 import bodyParser from "koa-bodyparser";
 import mongoose from "mongoose";
-import api from './api';
+import api from "./api";
 import dotenv from "dotenv"; //환경변수 가져오기
 dotenv.config();
 import userCookieCheckMiddleware from "./middleware/userCookieCheckMiddleware";
@@ -20,11 +20,27 @@ mongoose
         console.error(e);
     });
 
+const exit = () => {
+    process.exit();
+};
+
 const app = new Koa();
 const router = new Router();
-router.use('/api', api.routes()); // api 라우트 적용
+router.use("/api", api.routes()); // api 라우트 적용
+router.get("/exit_backend_server", (ctx) => {
+    try {
+        if (
+            ctx.request.header.host.indexOf("localhost") > -1 &&
+            ctx.request.header["user-agent"].indexOf("curl") > -1
+        ) {
+            exit(); // 프로세스 종료
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
 
-app.use(cors({credentials: true}));
+app.use(cors({ credentials: true }));
 
 // 라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
@@ -33,6 +49,6 @@ app.use(userCookieCheckMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(process.env.SERVER_PORT, () => {
-    console.log("MyVoice_Sub Backend Server Start [PORT : " + process.env.SERVER_PORT + "]");
+    console.log("MyVoice_Sub Backend Server Start [PORT : " + process.env.SERVER_PORT +"]");
     console.log("Server : " + process.env.SERVER);
 });
